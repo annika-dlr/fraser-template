1572b492bbcb5f37b00267b73076a8c6e49790b2
ofen_an <annika.ofenloch@dlr.de>
2017-11-13 16:43:34 +0100

    Updated script to execute all models locally (for testing)

--- scripts/startSimulation.sh
+++ scripts/startSimulation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-../bin/configServer &
-../bin/eventQueue &
-../bin/model1 &
-../bin/model2 &
-../bin/simulationModel
+../models/configuration_server/build/bin/configuration_server &
+../models/event_queue_1/build/bin/event_queue_1 &
+../models/model_1/build/bin/model_1 &
+../models/model_2/build/bin/model_2 &
+../models/simulation_model/build/bin/simulation_model
