c8c1e3f6a2f9b58f720527f4da1b35266a9e89a2
Annika Ofenloch <annika.ofenloch@dlr.de>
2017-12-11 14:33:50 +0100

    Added needed command line parameters

--- scripts/startSimulation.sh
+++ scripts/startSimulation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-../models/configuration_server/build/bin/configuration_server &
+../models/configuration_server/build/bin/configuration_server --models-config-file ../models/configuration_server/configuration/models-config.xml &
 ../models/event_queue_1/build/bin/event_queue_1 &
 ../models/model_1/build/bin/model_1 &
 ../models/model_2/build/bin/model_2 &
