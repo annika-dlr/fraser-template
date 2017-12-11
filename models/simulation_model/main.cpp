64a5e181a02d288ec8b86e62bfae506ba9e378df
Annika Ofenloch <annika.ofenloch@dlr.de>
2017-12-11 14:32:52 +0100

    Removed threads creation

--- models/simulation_model/main.cpp
+++ models/simulation_model/main.cpp
@@ -7,46 +7,23 @@
 
 #include <iostream>
 #include <string>
-#include <boost/chrono.hpp>
-#include <boost/thread.hpp>
 
 #include "SimulationModel.h"
 
-SimulationModel simulation("simulation_model", "Simulation Environment");
-std::string configFile = "";
-
-void startSimulationThread() {
-	try {
-		simulation.configure(configFile);
-		simulation.run();
-	} catch (boost::thread_interrupted&) {
-		std::cout << " SimulationModel: Interrupt received: Exit" << std::endl;
-	}
-}
-
-void createConfigurationFilesThread() {
-	try {
-		simulation.setConfigMode(true);
-		simulation.store(configFile);
-	} catch (boost::thread_interrupted&) {
-		std::cout << " SimulationModel: Interrupt received: Exit" << std::endl;
-	}
-}
-
 int main(int argc, char* argv[]) {
 	if (argc > 2) {
-		configFile = argv[2];
-		if (static_cast<std::string>(argv[1]) == "--create-config-files") {
-			std::cout
-					<< "Configuration Mode: Create default configuration files"
-					<< std::endl;
+		std::string configFile = argv[2];
+		SimulationModel simulation("simulation_model", "Simulation Environment");
 
-			boost::thread configThread(createConfigurationFilesThread);
-			configThread.join();
+		if (static_cast<std::string>(argv[1]) == "--create-config-files") {
+			std::cout << "Create default configuration files" << std::endl;
+			simulation.setConfigMode(true);
+			simulation.store(configFile);
+
 		} else if (static_cast<std::string>(argv[1]) == "--load-config") {
-			std::cout << "--------> Create simulation thread " << std::endl;
-			boost::thread simThread(startSimulationThread);
-			simThread.join();   // main thread waits for the thread t to finish
+			simulation.configure(configFile);
+			simulation.run();
+
 		} else {
 			std::cout << " Invalid argument/s: --help" << std::endl;
 		}
