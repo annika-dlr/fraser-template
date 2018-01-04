509710ca5825eab1853cde985a6e548ffc40134b
Annika Ofenloch <annika.ofenloch@dlr.de>
2018-01-04 13:32:47 +0100

    Changed path of configuration files

--- scripts/startSimulation.sh
+++ scripts/startSimulation.sh
@@ -4,4 +4,4 @@
 ../models/event_queue_1/build/bin/event_queue_1 &
 ../models/model_1/build/bin/model_1 &
 ../models/model_2/build/bin/model_2 &
-../models/simulation_model/build/bin/simulation_model --load-config ../configurations/config_1/
+../models/simulation_model/build/bin/simulation_model --load-config ../configurations/config_0/
