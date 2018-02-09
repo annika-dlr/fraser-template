9143528ec1f5158049ef3467d533154567a97c50
ofen_an <annika.ofenloch@dlr.de>
2018-02-09 13:45:58 +0100

    Deleted scripts which were used for testing purposes

--- scripts/startSimulation.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-../models/configuration_server/build/bin/configuration_server --models-config-file ../models/configuration_server/configuration/models-config.xml &
-../models/event_queue_1/build/bin/event_queue_1 &
-../models/model_1/build/bin/model_1 &
-../models/model_2/build/bin/model_2 &
-../models/simulation_model/build/bin/simulation_model --load-config ../configurations/config_0/
