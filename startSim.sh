ea339eb5d830408e8d55934eb2818dff9a6c6150
annika-dlr <annika.ofenloch@dlr.de>
2018-04-27 18:00:53 +0200

    Added start-scripts for debugging purpose -> print out standard output

---
+++ startSim.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+~/tmp_simulation/models/configuration_server/build/bin/configuration_server --config-file ~/tmp_simulation/hosts_config.xml &
+~/tmp_simulation/models/router_0/build/bin/router_0 2>> ~/tmp_simulation/models/router_0/log.txt &
+~/tmp_simulation/models/router_1/build/bin/router_1 2>> ~/tmp_simulation/models/router_1/log.txt &
+~/tmp_simulation/models/router_2/build/bin/router_2 2>> ~/tmp_simulation/models/router_2/log.txt &
+~/tmp_simulation/models/router_3/build/bin/router_3 2>> ~/tmp_simulation/models/router_3/log.txt &
+~/tmp_simulation/models/simulation_model/build/bin/simulation_model --load-config ~/tmp_simulation/configurations/config_0/
