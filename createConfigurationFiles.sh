5e830df4790f198be94ac852fb15e84fc2b93b84
annika-dlr <annika.ofenloch@dlr.de>
2018-05-03 16:59:47 +0200

    Adapted bash scripts for local execution

--- createConfigurationFiles.sh
+++ createConfigurationFiles.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-~/tmp_simulation/models/configuration_server/build/bin/configuration_server --config-file ~/tmp_simulation/hosts_config.xml &
-~/tmp_simulation/models/router_0/build/bin/router_0 2>> ~/tmp_simulation/models/router_0/log.txt &
-~/tmp_simulation/models/router_1/build/bin/router_1 2>> ~/tmp_simulation/models/router_1/log.txt &
-~/tmp_simulation/models/router_2/build/bin/router_2 2>> ~/tmp_simulation/models/router_2/log.txt &
-~/tmp_simulation/models/router_3/build/bin/router_3 2>> ~/tmp_simulation/models/router_3/log.txt &
-~/tmp_simulation/models/simulation_model/build/bin/simulation_model --create-config-files ../configurations/config_0/
+models/configuration_server/build/bin/configuration_server --config-file hosts-configs/config0.xml &
+models/event_queue_1/build/bin/event_queue_1 &
+models/router_0/build/bin/router_0 &
+models/router_1/build/bin/router_1 &
+models/router_2/build/bin/router_2 &
+models/router_3/build/bin/router_3 &
+models/simulation_model/build/bin/simulation_model --create-config-files configurations/config_0/
